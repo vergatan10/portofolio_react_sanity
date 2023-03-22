52fc7bb1c45ba601337d658ca939b1c748f2dbd8
Verga Tandika <64019419+vergatan10@users.noreply.github.com>
2023-03-22 23:14:58 +0700

    add sanity schemas

---
+++ sanity/schemas/index.js
@@ -0,0 +1,18 @@
+import testimonials from './testimonials'
+import works from './works'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+export const schemaTypes = [
+  testimonials,
+  works,
+  brands,
+  abouts,
+  experiences,
+  skills,
+  workExperience,
+  contact,
+]
